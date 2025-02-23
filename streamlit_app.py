a89f6a4de6f5a62b4554474db319c46f4eb2aa64
ainaluis <83398345+ainaluis@users.noreply.github.com>
2025-02-23 22:55:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def push_to_github(csv_path):
     repo = git.Repo(clone_path)
 
     repo.config_writer().set_value('user', 'name', 'Aina Luis')
-    repo.config_writer().set_value('user', 'email', 'ainaluis02@gmail.com")
+    repo.config_writer().set_value('user', 'email', 'ainaluis02@gmail.com')
                                    
     origin = repo.remotes.origin
     origin.pull('main')
